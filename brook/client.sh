f0c8f05a9a45001dbd21b02efe67312e95b4d0aa
Brendan <brendan.b.liu@gmail.com>
2018-02-04 19:13:47 +0800

    brook config

---
+++ brook/client.sh
@@ -0,0 +1,2 @@
+nohup ~/Brook/brook client -l 127.0.0.1:1087 -i 127.0.0.1 -s ipipipip:80 -p "password" --http > /dev/null 2>&1 &
+nohup ~/Brook/brook client -l 127.0.0.1:1086 -i 127.0.0.1 -s ipipipip:80 -p "password" > /dev/null 2>&1 &
---
+++ brook/server.sh
@@ -0,0 +1,1 @@
+nohup /root/brook/brook server -l :80 -p "password" > /dev/null 2>&1 &
